5fa8fe7f31b43d7851bd40665ef43bdf3444e501
IvanSlivenko <ivan8822@ukr.net>
2024-08-14 14:59:55 +0300

    14-08-2024-14-59

--- shop/templatetags/shop_tags.py
+++ shop/templatetags/shop_tags.py
@@ -7,5 +7,33 @@ register = template.Library()
 def get_subcategories(category):
     return Category.objects.filter(parent=category)
 
+@register.simple_tag()
+def get_sorted():
+    sortes = [
+       {
+           'title': 'Ціна',
+            'sorters': [
+                ('price', 'за зростанням'),
+                ('-price', 'за спаданням')
+            ]
+       },
+        {
+           'title': 'Колір',
+            'sorters': [
+                ('color', 'від А до Я'),
+                ('-color', 'від Я до А')
+            ]
+       },
+        {
+           'title': 'Розмір',
+            'sorters': [
+                ('size', 'за зростанням'),
+                ('-size', 'за спаданням')
+            ]
+       }, 
+    ]
+
+    return sortes
+
 
 
